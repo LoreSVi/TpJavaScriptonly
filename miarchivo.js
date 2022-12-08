e80a87668d06fc0c1e0e62bf27ba02220dd86209
LoreSVi <loresuvivas@gmail.com>
2022-12-08 07:14:30 -0300

    4to tp iniciando

---
+++ miarchivo.js
@@ -0,0 +1,91 @@
+const inputNombre = document.getElementById('nombre');
+const inputApellido = document.getElementById('apellido');
+const botonDatos = document.getElementById('botonDatos')
+
+botonDatos.onclick = () => {
+const usuario = {
+    nombre: inputNombre.value,
+    apellido: inputApellido.value
+}
+inputNombre.value = ''
+inputApellido.value = ''
+console.log(usuario)
+
+localStorage.setItem('InfoUsuario',JSON.stringify(usuario))
+}
+
+
+
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+
+let carrito = [];
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${product.precio} $</p>
+        `;
+
+        shopContent.append(content);
+
+        let comprar = document.createElement("button");
+        comprar.innerText = "comprar";
+        comprar.className = "comprar";
+
+        content.append(comprar);
+
+        comprar.addEventListener("click", () => {
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+            });
+            console.log(carrito);
+        });
+});
+
+verCarrito.addEventListener("click", () => {
+modalContainer.innerHTML = "";  
+modalContainer.style.display = "flex";
+const modalHeader = document.createElement("div");
+modalHeader.className = "modal-header"
+modalHeader.innerHTML = 
+`<h1 class="modal-header-title">Carrito</h1>
+`;
+modalContainer.append(modalHeader);
+
+const modalbutton = document.createElement("h1");
+modalbutton.innerText = "x";
+modalbutton.className = "modal-header-button";
+
+modalbutton.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
+
+modalHeader.append(modalbutton);
+
+carrito.forEach((product) => {
+let carritoContent = document.createElement("div");
+carritoContent.className = "modal-content";
+carritoContent.innerHTML = `
+<img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio} $</p>
+    `;
+
+    modalContainer.append(carritoContent);
+  });
+
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+  const totalCompra = document.createElement("div")
+  totalCompra.className = "total-content"
+  totalCompra.innerHTML =  `Total a pagar: ${total}$`;
+  modalContainer.append(totalCompra);
+});
+
