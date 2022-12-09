6a271da91769e92e888c20d5738be227054c6ffc
LoreSVi <loresuvivas@gmail.com>
2022-12-09 13:56:49 -0300

    cambios

--- miarchivo.js
+++ miarchivo.js
@@ -18,65 +18,95 @@ botonDatos.onclick = () => {
 
 //const boton = document.getElementById('top3');
 //const listaProductos = document.querySelector("#lista-productos");
+/*const boton = document.getElementById('todos');
+const listaProductos = document.querySelector("#lista-productos");
+
+boton.onclick = () => {
+    fetch('/productos/data.json')
+        .then((response) => response.json())
+        .then(data => {
+            console.log(data)
+            data.forEach((product) => {
+                const li = document.createElement('li')
+                li.innerHTML = `
+            <img src="${product.img}">
+            <h3>${product.nombre}</h3>
+            <p class="price">${product.precio} $</p>
+            `
+                listaProductos.append(li)
+            })
+        })
+*/
+
 
 const shopContent = document.getElementById("shopContent");
 const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modal-container");
 
-
 let carrito = [];
-
 productos.forEach((product) => {
     let content = document.createElement("div");
     content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${product.precio} $</p>
+        `;
+
+        shopContent.append(content);
+
+        let comprar = document.createElement("button");
+        comprar.innerText = "comprar";
+        comprar.className = "comprar";
+
+        content.append(comprar);
+
+        comprar.addEventListener("click", () => {
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+            });
+            console.log(carrito);
+        });
+});
+
+verCarrito.addEventListener("click", () => {
+modalContainer.innerHTML = '';
+const modalHeader = document.createElement("div");
+modalHeader.className = "modal-header"
+modalHeader.innerHTML = 
+`<h1 class="modal-header-title">Carrito</h1>
+`;
+modalContainer.append(modalHeader);
+
+const modalbutton = document.createElement("h1");
+modalbutton.innerText = "x";
+modalbutton.className = "modal-header-button";
+
+modalbutton.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
+
+modalHeader.append(modalbutton);
+
+carrito.forEach((product) => {
+let carritoContent = document.createElement("div");
+carritoContent.className = "modal-content";
+carritoContent.innerHTML = `
+<img src="${product.img}">
     <h3>${product.nombre}</h3>
     <p class="price">${product.precio} $</p>
     `;
 
-    shopContent.append(content);
+    modalContainer.append(carritoContent);
+  });
 
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar";
-    comprar.className = "comprar";
+  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
 
-     content.append(comprar);
-
-    comprar.addEventListener("click", () => {
-        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-            if (repeat) {
-                carrito.map((prod) => {
-                    if (prod.id === product.id) {
-                        product.cantida++;
-                    }
-                });
-        } else {
-        carrito.push({
-            id: product.id,
-            img: product.img,
-            nombre: product.nombre,
-            precio: product.precio,
-            cantidad: product.cantidad,
-        });
-        console.log(carrito);
-        console.log(carrito.length);
-        carritoCounter();
-        saveLocal();
-    }
-   });
+  const totalCompra = document.createElement("div")
+  totalCompra.className = "total-content"
+  totalCompra.innerHTML =  `Total a pagar: ${total}$`;
+  modalContainer.append(totalCompra);
 });
-        Toastify({
-            text: `Producto Agregado al carrito`,
-            gravity: "bottom",
-            duration: 2000,
-            position: "center",
-            style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)"
-            },
-        }).showToast()
-
-
- const saveLocal = () => {
-    localStorage.setItem("carrito",JSON.stringify(carrito));
- };
